f97ae06bb6d6bf187a19dc92270b045bdc0c4a9e
Austin Clarke <Austin Clarke>
2017-03-31 16:28:29 -0500

    initial commit

---
+++ app/src/main/java/com/ccf/dtsma_coding_krewe/MainActivity.java
@@ -0,0 +1,13 @@
+package com.ccf.dtsma_coding_krewe;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
